19763cf9bce49a2676cc2ffee9f58c8ee5306fc6
Kate Edge <i7426159@bournemouth.ac.uk>
2017-02-13 16:57:52 +0000

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,27 +1,56 @@
 #include <iostream>
-#include "matrix.h"
-
-using namespace std;
-
+#include <cstdlib>
+#include <string>
+#include "array.h"
+#include <array>
 int main()
 {
-    // creates a 4x4 matrix
-    typedef Matrix<int,2,1> myMatrix;
+    Array<int,5,2> intArray{1,2,3,4,5,6,7,8,9,10,11};
 
-//    //myMatrix matrix2;
+    Array<int,3,2> myArray;
+    myArray={1,2,3,4,5,6};
+    //myArray(1,1)=2;
 
-   //myMatrix.operator ()(0,1);
-//    myMatrix(1,2)=2;
-//    myMatrix(2,10)=3;
-//    myMatrix(2,2)=4;
+    myArray.print();
+    //std::cout<<myArray.data()<<"\n";
 
-    //myMatrix(0,1) = 5;
+    //std::array<int,5>a={1,2,3,4,5,7};
+    // copy array2
+    Array<int,5,2> array2=intArray;
 
+    //Array<int>::const_iterator begin=array2.begin();
+    //Array<int>::const_iterator end=array2.end();
 
-//    myMatrix.operator()(0,1);
-    //cout << matrix << endl;
+//    while(begin !=end)
+//    {
+//        std::cout<<*(begin++)<<" ";
+//    }
+    std::cout<<"\n";
+    try
+    {
 
+        std::cout<<"try to access element at -1 \n"<<intArray.at(1,2)<<"\n";
+    }
+    catch(std::exception const &ex)
+    {
+        std::cerr<<"Exception "<<ex.what()<<std::endl;
+    }
+    try
+    {
+        std::cout<<intArray.at(5,1)<<"\n";
+    }
+    catch(std::exception const &ex)
+    {
+        std::cerr<<"Exception "<<ex.what()<<std::endl;
+    }
+    try
+    {
 
-    //cout << myMatrix(0,1) << endl;
-    return 0;
+        std::cout<<"try to access element at 99 \n"<<array2.at(99,3)<<"\n";
+    }
+    catch(std::exception const &ex)
+    {
+        std::cerr<<"Exception "<<ex.what()<<std::endl;
+    }
+
 }
